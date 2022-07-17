719e80b3e49cec0a496f27d76f98ae2403d4727c
Eben-Success <aebenezer237@gmail.com>
2022-07-17 13:26:13 -0700

    update main

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 hImg, wImg,_ = img.shape
 boxes = pytesseract.image_to_boxes(img)
 for b in boxes.splitlines():
-    print(b)
+    # print(b)
     b = b.split(' ')
     print(b)
     x,y,w,h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+    cv.rectangle(img, (x,hImg-y), (w,hImg-h), (0,0,255), 3)
+    cv.putText(img,b[0], (x,hImg-y+35), cv.FONT_HERSHEY_COMPLEX, 1,(50,50,255), 2)
+
 
 cv.imshow('Result', img)
 cv.waitKey(0)
