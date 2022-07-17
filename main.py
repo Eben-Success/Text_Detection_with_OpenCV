2ca9f4313af421795b71a91369afdc3d484d541b
Eben-Success <aebenezer237@gmail.com>
2022-07-17 13:02:10 -0700

    update main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 img = cv.imread('img.png')
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-print(pytesseract.image_to_string(img))
+# print(pytesseract.image_to_string(img))
+print(pytesseract.image_to_boxes(img))
 cv.imshow('Result', img)
 cv.waitKey(0)
